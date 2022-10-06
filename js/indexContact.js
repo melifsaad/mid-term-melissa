ebca457629e4cfc9f4f641ec65de3b4067b505d5
melifsaad <113357597+melifsaad@users.noreply.github.com>
2022-10-06 09:27:39 +0200

    repo

--- js/indexContact.js
+++ js/indexContact.js
@@ -1,5 +1,23 @@
-const infoName = document.querySelector ("#input1")
-document.querySelector ("#submit").addEventListener('click', () => {
-    
-    console.log (infoName.value)
-})
+const infoName = document.querySelector("#input1");
+document.querySelector("#submit").addEventListener("click", () => {
+  submit.preventDefault();
+  console.log(infoName.value);
+});
+
+// const infoEmail = document.querySelector ("#input2");
+// document.querySelector ("#submit").addEventListener('click', () => {
+//     submit.preventDefault();
+//     console.log (infoEmail.value)
+// })
+
+// const infoPhone = document.querySelector ("#input3");
+// document.querySelector ("#submit").addEventListener('click', () => {
+//     submit.preventDefault();
+//     console.log (infoPhone.value)
+// })
+
+// const infoMessage = document.querySelector ("#input4");
+// document.querySelector ("#submit").addEventListener('click', () => {
+//     submit.preventDefault();
+//     console.log (infoMessage.value)
+// })
